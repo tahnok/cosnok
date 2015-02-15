30d60ca5ff91fe65526b262fb6aa2c330914b0ad
Wesley Ellis <tahnok@gmail.com>
2015-02-15 14:28:32 -0500

    plugin structure

--- bot.rb
+++ bot.rb
@@ -1,14 +1,19 @@
 require 'cinch'
 
+plugins_to_load = []
+
+Dir['plugins/*.rb'].each do |plugin|
+  load plugin
+  plugins_to_load << Object.const_get(File.basename(plugin, '.rb').capitalize)
+end
+
 bot = Cinch::Bot.new do
   configure do |c|
     c.server = "irc.freenode.org"
     c.channels = ["#mctest"]
     c.nick = 'cosnok'
-  end
-
-  on :message, "hello" do |m|
-    m.reply "Hello, #{m.user.nick}"
+    c.plugins.plugins = plugins_to_load
+    c.plugins.prefix = /^\$/
   end
 end
 
