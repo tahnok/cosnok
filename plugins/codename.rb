f273bd91318daf673feec0d20344d105d78f780d
Wesley Ellis <tahnok@gmail.com>
2015-02-20 21:56:38 -0500

    make codename better

--- plugins/codename.rb
+++ plugins/codename.rb
@@ -8,35 +8,31 @@ class Codename
   end
 
   def make_codename
-    "#{prefix}#{random_nsa}#{random_nsa}"
+    "#{prefix}#{endpart}"
+  end
+
+  def endpart
+    if Random.rand > 0.5
+      "#{random("nsa")} #{random("nsa")}"
+    else
+      "#{random("adverbs")} #{random("gods")}"
+    end
   end
 
   def prefix
     if Random.rand > 0.3
       ""
     else
-      random_colour + " "
+      if Random.rand < 0.3
+        random("colours") + " "
+      else
+        random("adjectives") + " "
+      end
     end
   end
 
-  def random_colour
-    random_line("data/colours")
-  end
-
-  def random_nsa
-    random_line("data/nsa")
-  end
-
-  def random_adverb
-    random_line("data/adverbs")
-  end
-
-  def random_adjective
-    random_line("data/adjectives")
-  end
-
-  def random_noun
-    random_line("data/nouns")
+  def random(thing)
+    random_line("data/#{thing}")
   end
 
   def random_line(path)
