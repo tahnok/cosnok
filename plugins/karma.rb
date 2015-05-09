64b0df463b74cde6765697c9025ff4cacb87884e
Wesley Ellis <tahnok@gmail.com>
2015-05-08 21:00:34 -0400

    karma tracking for lulz

---
+++ plugins/karma.rb
@@ -0,0 +1,32 @@
+require 'redis'
+
+class Karma
+  include Cinch::Plugin
+
+  set :prefix, //
+
+  match /(\w+)(\+\+|\-\-)/
+
+  def execute(m, user, direction)
+    redis = Redis.new(url: ENV['REDISCLOUD_URL'])
+    return unless track_karma?(redis, m, user)
+    karma = redis.get("karma:#{user}").to_i || 0
+    if direction == "++"
+      karma = karma + 1
+    else
+      karma = karma - 1
+    end
+    redis.sadd("nicks:#{m.channel.name}", user)
+    redis.set("karma:#{user}", karma)
+  end
+
+  def help
+    "karma - in beta"
+  end
+
+  def track_karma?(redis, m, user)
+    m.user.nick != user && (m.channel.users.keys.include?(user) || redis.sismember("nicks:#{m.channel.name}", user))
+  end
+
+
+end
