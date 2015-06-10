de3859df98c8d8edab3e768c8c125047007f768a
Wesley Ellis <tahnok@gmail.com>
2015-06-10 15:26:14 -0400

    typos

--- plugins/karmecheladder.rb
+++ plugins/karmecheladder.rb
@@ -11,13 +11,13 @@ class Karmecheladder
     when 1
       "wow! your first karma! it's so shinny and useful! you climb the ladder to the GREENTIKE rung!"
     when 10
-      "quite a chunk of karma you've got there! you now perch atop the mildly respectable KNEEHIGH RASCALLY SCALLYWAG"
+      "quite a chunk of karma you've got there! you now perch atop the mildly respectable KNEEHIGH RASCALLY SCALLYWAG rung"
     when 20
       "If karma was worth anything, you'd be doing great! Congrats on reaching the FLEDGLING JOKE CRAFTER rung"
     when 42
       "Weren't expecting a new rung now were you?! SUPRISE! You are now a JUNIOR FUNTIME JUNIOR"
     when 50
-      "Halfway to a 100! I bet that feels good. You are hereby awarded the SEMI SERIOUS KARMA PERSON"
+      "Halfway to a 100! I bet that feels good. You are hereby awarded the title SEMI SERIOUS KARMA PERSON"
     when 100
       "100! that is more 0 karma by a lot! the coveted AMUSING IRC DENIZEN rung is yours!"
     when 200
