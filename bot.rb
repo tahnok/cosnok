dfc42aa20ae5f7016dbcd41f78c733fbce13142f
Wesley Ellis <tahnok@gmail.com>
2016-06-02 20:43:08 -0400

    cleanup bot.rb

--- bot.rb
+++ bot.rb
@@ -14,14 +14,12 @@ end
 bot = Cinch::Bot.new do
   configure do |c|
     c.server = "127.0.0.1"
-    c.channels = ["#tildetown", "bots"]
+    c.channels = ["#tildetown", "#bots"]
+    c.port = 8866
     c.nick = botname
+    c.user = botname
     c.plugins.plugins = plugins_to_load
     c.plugins.prefix = /^#{botname}_*\:? ?/
-  end
-
-  on :connect do |m|
-    User('nickserv').send("identify #{botname} #{ENV['NICKSERV_PASS']}")
   end
 
 end
