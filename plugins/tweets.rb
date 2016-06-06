8756b6a1f918752db83909cc3f4829585fa4d421
Wesley Ellis <tahnok@gmail.com>
2016-06-06 09:59:20 -0400

    actually fix tweet newlines

--- plugins/tweets.rb
+++ plugins/tweets.rb
@@ -8,7 +8,7 @@ class Tweets
 
   def execute(m,id)
     status = twitter.status(id)
-    m.reply "[\"#{status.text.join('\n ')}\" @#{status.user.screen_name}]"
+    m.reply "[\"#{status.text.gsub(/\n/, ' \n ')}\" @#{status.user.screen_name}]"
   end
 
   def help
